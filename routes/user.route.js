7b2d25fa8f499a50d441e209d0eb47e425b77719
jentr3s <jentr3s03@gmail.com>
2018-09-26 13:08:24 +0800

    Initial commit

---
+++ routes/user.route.js
@@ -0,0 +1,111 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user.model');
+
+const tokenList = {}
+
+router.post('/signup', function (req, res) {
+    console.log(req.body);
+    bcrypt.hash(req.body.password, 10, function (err, hash) {
+        if (err) {
+            return res.status(500).json({
+                error: err
+            });
+        }
+        else {
+            const user = new User({
+                _id: new mongoose.Types.ObjectId(),
+                email: req.body.email,
+                password: hash
+            });
+            user.save().then(function (result) {
+                console.log(result);
+                res.status(200).json({
+                    success: 'New user has been created'
+                });
+            }).catch(error => {
+                res.status(500).json({
+                    error: err
+                });
+            });
+        }
+    });
+});
+
+router.post('/signin', (req, res) => {
+    User.findOne({ email: req.body.email })
+        .exec()
+        .then(function (user) {
+            bcrypt.compare(req.body.password, user.password, function (err, result) {
+                if (err) {
+                    return res.status(401).json({
+                        failed: 'Unauthorized Access'
+                    });
+                }
+                if (result) {
+                    const JWTToken = jwt.sign({
+                        email: user.email,
+                        _id: user._id
+                    },
+                        'secret',
+                        {
+                            expiresIn: '2h'
+                        });
+
+                    const refreshToken = jwt.sign({
+                        email: user.email,
+                        _id: user._id
+                    }, 'secret', { expiresIn: '5h' })
+
+
+                    tokenList[refreshToken] = refreshToken
+                    return res.status(200).json({
+                        success: 'Welcome to the JWT Auth',
+                        token: JWTToken,
+                        refreshToken: refreshToken
+                    });
+                }
+                return res.status(401).json({
+                    failed: 'Unauthorized Access'
+                });
+            });
+        })
+        .catch(error => {
+            res.status(500).json({
+                error: error
+            });
+        });
+});
+
+router.post('/token', (req, res) => {
+    const postData = req.body
+    if ((postData.refreshToken) && (postData.refreshToken in tokenList)) {
+        const refreshToken = jwt.sign({
+            email: user.email,
+            _id: '5ba9cfa0d1cc8733f407b186'
+        },
+            'secret',
+            {
+                expiresIn: '2h'
+            });
+
+        // update the token in the list
+        tokenList[postData.refreshToken].token = token
+        return res.status(200).json({
+            success: 'Refresh Token',
+            refreshToken: refreshToken
+        });
+    }
+});
+
+router.get('/secure', (req, res) => {
+    // all secured routes goes here
+    res.send('I am secured...')
+})
+
+router.use(require('../tokenChecker'))
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const user = require('./routes/user.route');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/jwtauth', {
+    useNewUrlParser: true
+})
+const PORT = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/checking', function (req, res) {
+    res.json({
+        "Tutorial": "Welcome to the Node express JWT Tutorial"
+    });
+});
+
+app.use('/user', user);
+
+app.listen(PORT, function () {
+    console.log('Server is running on Port', PORT);
+});
